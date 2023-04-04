53ee5122dd71264368cf1c5bf007ebdba72551b8
rMaiaa <ronaldteofilomaia@gmail.com>
2023-04-04 20:35:07 -0300

    CRUD funcionando.

--- Academico/Controllers/InstituicaoController.cs
+++ Academico/Controllers/InstituicaoController.cs
@@ -38,7 +38,20 @@ namespace Academico.Controllers
 
         public IActionResult Details(long id)
         {
+            return View(instituicoes.Where(i => i.Id == id).First());
+        }
 
+        public IActionResult Delete(long id)
+        {
+            return View(instituicoes.Where(i => i.Id == id).First());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Instituicao instituicao)
+        {
+            instituicoes.Remove(instituicoes.Where(i => i.Id == instituicao.Id).First());
+            return RedirectToAction("Index");
         }
     }
 }
